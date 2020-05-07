c3e3bc13de9bdffe2368a10b5cce3c73ffe1a725
Mikhail Titov <mlt@gmx.us>
2020-05-07 18:41:27 -0500

    Updated dist

--- dist/ekko-lightbox.js
+++ dist/ekko-lightbox.js
@@ -48,8 +48,8 @@ var Lightbox = (function ($) {
 			key: 'Default',
 
 			/**
-       Class properties:
-   	 _$element: null -> the <a> element currently being displayed
+   	    Class properties:
+   		 _$element: null -> the <a> element currently being displayed
     _$modal: The bootstrap modal generated
        _$modalDialog: The .modal-dialog
        _$modalContent: The .modal-content
@@ -60,13 +60,13 @@ var Lightbox = (function ($) {
     _$lightboxContainerTwo: Container of the second lightbox element
     _$lightboxBody: First element in the container
     _$modalArrows: The overlayed arrows container
-   	 _$galleryItems: Other <a>'s available for this gallery
+   		 _$galleryItems: Other <a>'s available for this gallery
     _galleryName: Name of the current data('gallery') showing
     _galleryIndex: The current index of the _$galleryItems being shown
-   	 _config: {} the options for the modal
+   		 _config: {} the options for the modal
     _modalId: unique id for the current lightbox
     _padding / _border: CSS properties for the modal container; these are used to calculate the available space for the content
-   	 */
+   		 */
 
 			get: function get() {
 				return Default;
@@ -502,7 +502,7 @@ var Lightbox = (function ($) {
 				// local ajax can be loaded into the container itself
 				if (!disableExternalCheck && !this._isExternal(url)) {
 					$containerForElement.load(url, $.proxy(function () {
-						return _this3._$element.trigger('loaded.bs.modal');l;
+						return _this3._$element.trigger('loaded.bs.modal');
 					}));
 				} else {
 					$containerForElement.html('<iframe src="' + url + '" frameborder="0" allowfullscreen></iframe>');
@@ -670,7 +670,9 @@ var Lightbox = (function ($) {
 				config = config || {};
 				return this.each(function () {
 					var $this = $(_this5);
-					var _config = $.extend({}, Lightbox.Default, $this.data(), typeof config === 'object' && config);
+					var data = $this.data();
+					delete data.remote;
+					var _config = $.extend({}, Lightbox.Default, data, typeof config === 'object' && config);
 
 					new Lightbox(_this5, _config);
 				});
